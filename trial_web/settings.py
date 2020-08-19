9e17856171dded70d3a6d048144984fe26f0a39c
SYLVAIN YABRE <yabre94854@SYLVAIN-YABRE.local>
2020-08-20 05:10:27 +0800

    whitenoise middleware added

--- trial_web/settings.py
+++ trial_web/settings.py
@@ -42,6 +42,7 @@ AUTH_USER_MODEL = 'registration.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
