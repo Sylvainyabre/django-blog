c43e6cd64f6a7b52336a3cf29e1ea70d317351d3
SYLVAIN YABRE <yabre94854@SYLVAIN-YABRE.local>
2020-08-20 04:35:04 +0800

    static file dir changed

--- trial_web/settings.py
+++ trial_web/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 #STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'my_site/static/'), ]
 
 # uploaded media goes here
 MEDIA_URL = '/media/'
