b3f01e653a2358288e7727cb77959a56e9b47ff8
SYLVAIN YABRE <yabre94854@SYLVAIN-YABRE.local>
2020-08-20 17:19:51 +0800

    static roots disabled

--- trial_web/settings.py
+++ trial_web/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # uploaded media goes here
 # MEDIA_URL = '/media/'
