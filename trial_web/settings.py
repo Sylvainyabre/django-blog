6c3cd038c792f911aaf9c512b7f32fff18c68b41
SYLVAIN YABRE <yabre94854@SYLVAIN-YABRE.local>
2020-08-23 02:40:43 +0800

    static and media now loading from s3

--- trial_web/settings.py
+++ trial_web/settings.py
@@ -118,10 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-# uploaded media goes here
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Twilio Sendgris email settings
@@ -139,32 +135,31 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
+AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_IS_GZIPPED = True
 
 # telling django to look for the static files in a directory called "static"
-AWS_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+#AWS_LOCATION = 'static'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATIC_URL = '/static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_ROOT = STATIC_URL
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATIC_URL = '/static/'
+# uploaded media goes here
+MEDIA_URL = '/media/'
+MEDIA_ROOT = MEDIA_URL
+#MEDIA_LOCATION = 'media'
+#os.path.join(BASE_DIR, 'media')
 
-MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
-
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-
-#STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder',
-                       #'django.contrib.staticfiles.finders.AppDirectoriesFinder',)
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'home'
@@ -186,4 +181,4 @@ CKEDITOR_CONFIGS = {
 
 }
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(),staticfiles=False)
