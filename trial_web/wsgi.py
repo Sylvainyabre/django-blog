3d543b38bd433a64847c23d1f782f3c26d032a7a
SYLVAIN YABRE <yabre94854@SYLVAIN-YABRE.local>
2020-08-23 15:26:03 +0800

    .production added to wsgi file

--- trial_web/wsgi.py
+++ trial_web/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trial_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trial_web.settings.production')
 
 application = get_wsgi_application()
