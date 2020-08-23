95dab8ebc69a172405c231959b3b3fa900e8f0ee
SYLVAIN YABRE <yabre94854@SYLVAIN-YABRE.local>
2020-08-23 15:37:17 +0800

    STATI_DIRS commented

--- trial_web/settings.py
+++ trial_web/settings.py
@@ -144,7 +144,7 @@ AWS_IS_GZIPPED = True
 #AWS_LOCATION = 'static'
 # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_ROOT = STATIC_URL
 
