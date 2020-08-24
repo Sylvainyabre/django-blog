ec14ff45ad9d8221673979c44ce1d91248d0a845
SYLVAIN YABRE <yabre94854@SYLVAIN-YABRE.local>
2020-08-24 18:25:09 +0800

    turning to whitenoise

--- trial_web/settings.py
+++ trial_web/settings.py
@@ -41,6 +41,7 @@ AUTH_USER_MODEL = 'registration.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,24 +142,24 @@ AWS_DEFAULT_ACL = None
 AWS_IS_GZIPPED = True
 
 # telling django to look for the static files in a directory called "static"
-#AWS_LOCATION = 'static'
-#STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# AWS_LOCATION = 'static'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = STATIC_URL
+# STATIC_ROOT = STATIC_URL
 
 
 # uploaded media goes here
 #
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = MEDIA_URL
-#MEDIA_LOCATION = '/static/media'
+# MEDIA_LOCATION = '/static/media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_OBJECT_PARAMETERS = {
@@ -185,4 +186,5 @@ CKEDITOR_CONFIGS = {
 
 }
 
-django_heroku.settings(locals(), staticfiles=False)
+django_heroku.settings(locals())
+# staticfiles=False
