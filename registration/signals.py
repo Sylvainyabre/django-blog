af60497fb178a07b30e9505affedb8607ba43e45
SYLVAIN YABRE <yabre94854@SYLVAIN-YABRE.local>
2020-08-28 14:36:46 +0800

    csrf cookie secure set to false

--- registration/signals.py
+++ registration/signals.py
@@ -9,5 +9,7 @@ def create_user_profile(sender, instance,created, **kwargs):
     if created:
         Profile.objects.create(user =instance)
 
+    instance.Profile.save()
+
 
 
--- registration/views.py
+++ registration/views.py
@@ -78,3 +78,5 @@ def profile_update(request):
 
 
 
+
+
