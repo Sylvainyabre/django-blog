0d207a6728aa2c6e187af1958a0b57bda83fac8e
SYLVAIN YABRE <yabre94854@SYLVAIN-YABRE.local>
2020-09-01 11:36:33 +0800

    prism js added, attempt to address signal malfunction

--- registration/signals.py
+++ registration/signals.py
@@ -1,15 +1,16 @@
 from django.db.models.signals import post_save
 from .models import Profile
 from django.contrib.auth.models import User
+from .models import CustomUser
 from django.dispatch import receiver
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=CustomUser)
 def create_user_profile(sender, instance,created, **kwargs):
     if created:
         Profile.objects.create(user =instance)
 
-    instance.Profile.save()
+        instance.Profile.save()
 
 
 
